55aaa327e46b2c7a8ec3f82e640a8eb5891bcf04
Skaiiwalker <lbar8299@740s-pl-119.local>
2015-12-16 09:26:14 -0700

    added getter and setters

--- src/bot/controller/LukeBotController.java
+++ src/bot/controller/LukeBotController.java
@@ -1,9 +1,67 @@
 package bot.controller;
 
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+import bot.model.EV3Robot;
+
 public class LukeBotController 
 {
+	private String message;
+	private int xPosition, yPosition;
+	private long waitTime;
+	
+	public LukeBotController()
+	{
+		this.xPosition = 50;
+		this.yPosition = 100;
+		this.waitTime = 4000;
+		this.message = "Something about a robot";
+		
+	}
+	
 	public void start()
 	{
-		
+		LCD.drawString(message, xPosition, yPosition);
+	}
+
+	public String getMessage() 
+	{
+		return message;
+	}
+
+	public int getxPosition() 
+	{
+		return xPosition;
+	}
+
+	public int getyPosition() 
+	{
+		return yPosition;
+	}
+
+	public long getWaitTime() 
+	{
+		return waitTime;
+	}
+
+	public void setMessage(String message) 
+	{
+		this.message = message;
+	}
+
+	public void setxPosition(int xPosition) 
+	{
+		this.xPosition = xPosition;
+	}
+
+	public void setyPosition(int yPosition) 
+	{
+		this.yPosition = yPosition;
+	}
+
+	public void setWaitTime(long waitTime) 
+	{
+		this.waitTime = waitTime;
 	}
 }
